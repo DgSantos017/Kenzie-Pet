14b2792a872aab748a8d9275e77af15d52c51b18
Diogo <diogosantosferreira.01@gmail.com>
2021-10-28 10:08:33 -0300

    feat: corrigindo DR

--- animais/models.py
+++ animais/models.py
@@ -1,14 +1,16 @@
 from django.db import models
 
-class Group(models.Model):
-    name = models.CharField(max_length=255)
-    scientific_name = models.CharField(max_length=255)
-
 class Animal(models.Model):
     name = models.CharField(max_length=255)
     age = models.FloatField() 
     weight = models.FloatField()  
     sex = models.CharField(max_length=255)
 
+class Group(models.Model):
+    name = models.CharField(max_length=255)
+    scientific_name = models.CharField(max_length=255)
+    animais = models.ForeignKey("Animal", on_delete=models.CASCADE)
+
 class Characteristic(models.Model):
     name = models.CharField(max_length=255)
+    animais = models.ManyToManyField(Animal)
