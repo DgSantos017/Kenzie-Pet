f17f64f47548e21a437a6097beba8831f1aba570
Diogo <diogosantosferreira.01@gmail.com>
2021-11-01 09:44:39 -0300

    feat: METHODOS GET and DELETE

--- animais/views.py
+++ animais/views.py
@@ -23,12 +23,7 @@ class AnimalView(APIView):
 
         group = Group.objects.get(scientific_name=request_group['scientific_name'])
 
-        animal = Animal(
-            name=data['name'], 
-            age=data['age'], 
-            weight=data['weight'], 
-            sex=data['sex'], 
-            group=group)
+        animal = Animal(name=data['name'], age=data['age'], weight=data['weight'], sex=data['sex'], group=group)
         animal.save()
 
         for characteristic in characteristics:
@@ -51,4 +46,28 @@ class AnimalView(APIView):
         return Response(serializer.errors)
 
 
-    
+    def get(self, request, animal_id=''):
+        if animal_id:
+            try:
+                animais = Animal.objects.get(id=animal_id)
+                serializer = AnimalSerializer(animais)
+                return Response(serializer.data)
+
+            except ObjectDoesNotExist:
+                return Response({'error': 'Animal not found'}, status=404)
+
+        animais = Animal.objects.all()
+        serializer = AnimalSerializer(animais, many=True)
+        return Response(serializer.data)
+
+
+
+    def delete(self, request, animal_id):
+        try:
+            animal = Animal.objects.get(id=animal_id)
+            animal.delete()
+            return Response(status=204)
+
+        except ObjectDoesNotExist:
+            return Response({'error': 'Animal not found'}, status=404)
+
