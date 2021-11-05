d6943ead3daf8e3875b06e3d24ea97a8df6dc441
Diogo <diogosantosferreira.01@gmail.com>
2021-11-05 14:03:33 -0300

    fix: corrigindo urls

--- animais/urls.py
+++ animais/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import AnimalView
 
 urlpatterns = [
-    path('animais/', AnimalView.as_view()),
-    path('animais/<int:animal_id>/', AnimalView.as_view()),
+    path('animals/', AnimalView.as_view()),
+    path('animals/<int:animal_id>/', AnimalView.as_view()),
 ]
